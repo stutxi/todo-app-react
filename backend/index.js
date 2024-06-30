3b5e99bdbc4f510c19d5e8cc5a14c7994b3762ad
Stuti Sharma <stuti105sharma@gmail.com>
2024-06-30 12:34:14 +0530

    intial commit and backend done

---
+++ backend/index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const { createTodo } = require("./types");
+const { todo } = require("./db");
+
+const app = express();
+
+
+app.use(express.json());
+
+app.post("/todo", async (req, res) => {
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if (!parsedPayload.success) {
+        res.status(411).json({
+            message: "You sent the wrong inputs",
+        })
+        return;
+    }
+    //put it in mongodb
+    await todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
+        completed: false
+    })
+    res.json({
+        message: "Todo created"
+    })
+})
+
+app.get("/todos", async (req, res) => {
+    const todos = await todo.find({});
+    res.json({
+        todos
+    })
+})
+
+app.put("/completed", async (req, res) => {
+    const updatePayload = req.body;
+    const parsePayload = updateTodo.safeParse(updatePayload);
+    if (!parsePayload.success) {
+        res.status(411).json({
+            message: "You sent the wrong inputs",
+        })
+        return;
+    }
+    await todo.update({
+        _id: req.body.id
+    }, {
+        completed: true
+    })
+    res.json({
+        message: "Todo marked as completed"
+    })
+})
