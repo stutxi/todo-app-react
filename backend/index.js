7c1555a0bbf14ba26ec7489e5e0cc0d39853ba5a
Stuti Sharma <stuti105sharma@gmail.com>
2024-06-30 16:49:02 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const { createTodo } = require("./types");
+const { createTodo, updateTodo } = require("./types");
 const { todo } = require("./db");
+const cors = require("cors");
 
 const app = express();
 
 
 app.use(express.json());
+app.use(cors());
 
 app.post("/todo", async (req, res) => {
     const createPayload = req.body;
